a87bc457706017257ec8cf70e15ac5fac58e2720
AirGamming <41245831+AirGamming@users.noreply.github.com>
2023-07-30 18:31:26 +0200

    initial rebuild

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import Footer from "./components/footer";
+import App from "./app.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-		<Footer />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
