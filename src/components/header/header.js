ef58ce2f47107336c5876a894f154c8077aa65cb
AirGamming <41245831+AirGamming@users.noreply.github.com>
2023-08-08 12:47:27 +0200

    header menu added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../css/header.css";
+import Menu from "./menu.js"
+
 export default function HeaderExp() {
   return (
 		<header>
@@ -7,6 +9,7 @@ export default function HeaderExp() {
 				<i class="fa-solid fa-file-pen"></i>
 				<h1>CV generator</h1>
 			</div>
+			 <Menu />
 		</header>
 	);
 }
---
+++ src/components/header/menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Menu() {    
+    return (
+        <ul>
+            <li id="active">Generator</li>
+            <li>About</li>
+        </ul>   
+    )
+}
